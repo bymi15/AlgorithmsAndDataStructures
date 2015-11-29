9094618b7b8f26d23d128e1522f9690cbbe76cde
Brian Min <bymi15@yahoo.com>
2015-11-29 23:25:56 +0000

    Update QuadraticPrimes.java

--- QuadraticPrimes.java
+++ QuadraticPrimes.java
@@ -31,6 +31,7 @@ public class Solution {
     }
     
     private static boolean isPrime(int n){
+        if(n == 2) return true;
         if(n % 2 == 0) return false;
         for(int i = 3; i*i <= n; i+=2){
             if(n % i == 0){
